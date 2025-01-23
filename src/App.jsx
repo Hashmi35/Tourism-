8ce50c37b8181112bac23c44e3f93a137fd0d5f9
Hashmi <ahasmigamer2@gmail.com>
2025-01-23 09:59:29 +0700

    Initial commit - project start

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Header()
+{
+  return (
+    <header>
+      
+    </header>
+  )
+}
+
+function Main()
+{
+  return(
+    <h1>sup dude</h1>
+  )
+}
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+     <Main   />
+     </>
+  )
+}
+
+
+export default App
